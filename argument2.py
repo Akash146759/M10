516e3b7b435e2082cc0a352c5dff941d8bd9d739
Akash146759 <akash.ganguly13@gmail.com>
2025-03-14 21:23:25 +0530

    3/14/2025

---
+++ argument2.py
@@ -0,0 +1,9 @@
+# function to find cube
+def cube(num):
+    answer = num * num * num
+    return answer
+
+num = 6
+
+print(f"The cube of {num} is {cube(num)}")
+print('the cube of', num , 'is', cube(num))
---
+++ rough.py
@@ -0,0 +1,10 @@
+def add(P,Q):
+    addition = P + Q
+    print(addition)
+
+def sub(P,Q):
+    subtraction = P - Q
+    print()
+
+num1 = input
+num2 = input
